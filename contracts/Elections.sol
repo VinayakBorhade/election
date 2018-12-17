b0f7caa712b25e4e852176089b9a1bb34e983901
Vin_04 <vinayakborhade@gmail.com>
2018-12-17 16:56:33 +0530

    list-candidates

--- contracts/Elections.sol
+++ contracts/Elections.sol
@@ -1,11 +1,26 @@
 pragma solidity ^0.4.11;
 
 contract Election{
-	//store candidate
-	//read candidate
-	string public candidate;
-	//constructor
+	//model candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	//store candidates
+	//fetch candidates
+	mapping(uint=>Candidate) public candidates;
+	//store candidate count
+	uint public candidatesCount;
+
 	function Election () public {
-		candidate="Candidate 1";
+		addCandidate("candidate1");
+		addCandidate("candidate2");
+	}
+
+	function addCandidate (string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount]=Candidate(candidatesCount, _name,0);
+
 	}
 }
