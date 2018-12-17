72634688b58d3c3bd0c56beaa9bf27bac8abd5c9
Vin_04 <vinayakborhade@gmail.com>
2018-12-17 19:46:30 +0530

    Cast Votes

--- contracts/Elections.sol
+++ contracts/Elections.sol
@@ -7,6 +7,8 @@ contract Election{
 		string name;
 		uint voteCount;
 	}
+	//Store accounts that have voted
+	mapping(address => bool) public voters;
 	//store candidates
 	//fetch candidates
 	mapping(uint=>Candidate) public candidates;
@@ -21,6 +23,16 @@ contract Election{
 	function addCandidate (string _name) private {
 		candidatesCount++;
 		candidates[candidatesCount]=Candidate(candidatesCount, _name,0);
+	}
 
+	function vote (uint _candidateId) public {
+		//require that they haven't voted before
+		require(!voters[msg.sender]);
+		//require a vaild candidate
+		require(_candidateId>0 && _candidateId<=candidatesCount);
+		//record that voter has voted
+		voters[msg.sender]=true;
+		//update candidate vote count
+		candidates[_candidateId].voteCount++;
 	}
 }
